661e5c47b95e2851fe4bca2d59614307a664ac2a
Cristina <cjimenez922@gmail.com>
2024-01-15 12:57:07 +0100

    create user-form component

--- frontend/usersapp/src/app/app.module.ts
+++ frontend/usersapp/src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './component/user-list/user-list.component';
+import { UserFormComponent } from './component/user-form/user-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [
