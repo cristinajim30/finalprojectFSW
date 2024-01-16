0ae2c879ed7c56eaf9b3f0db15a89bd3221db80d
Cristina <cjimenez922@gmail.com>
2024-01-16 11:40:55 +0100

    test 1 working for user-form

--- angular/usersapp/src/app/component/user-form/user-form.component.ts
+++ angular/usersapp/src/app/component/user-form/user-form.component.ts
@@ -11,16 +11,51 @@ import { UserServiceService } from '../../services/user-service.service';
 export class UserFormComponent {
 
   user: User;
+  validName=true
+  validEmail=true
 
   constructor(private route: ActivatedRoute, private router: Router, private userService: UserServiceService){
     this.user = new User();
   }
 
   onSubmit(){
-    this.userService.save(this.user).subscribe(result => this.gotoUserList())
+    this.validateInputs()
+    if (this.validName && this.validEmail){
+      this.userService.save(this.user).subscribe(result => this.gotoUserList())
+    }
+    
   }
 
   gotoUserList(){
     this.router.navigate(['/users']);
+  }
+
+  validateInputs(){
+    console.log("name: ", this.user.name)
+    switch (this.user.name){
+      case undefined: { 
+        console.log("name undefined: ", this.user.name ," ", this.user.email)
+        this.validName= false
+        break; 
+     } 
+     case " ": { 
+      console.log("name with spaces: ", this.user.name ," ", this.user.email )
+      this.validName= false
+        break; 
+     } 
+    }
+    switch (this.user.email){
+      case undefined: { 
+        console.log("email undefined: ", this.user.email)
+        this.validEmail= false
+        break; 
+     } 
+     case " ": { 
+      console.log("email with spaces: ",  this.user.email )
+      this.validEmail= false
+        break; 
+     } 
+    }
+    
   }
 }
